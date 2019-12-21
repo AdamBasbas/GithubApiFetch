4e304e1b7ed779f6070419d82d62758ba2b08673
Adam Basbas <basbas602@hotmail.com>
2019-12-21 20:21:50 +0100

    Create Api interface provide api url provide get request

---
+++ app/src/main/java/com/example/githubapifetch/Api.java
@@ -0,0 +1,15 @@
+package com.example.githubapifetch;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.Call;
+
+public interface Api {
+
+    String BASE_URL="https://api.github.com/";
+
+    @GET("search/repositories?q=created:>2017-10-22&sort=stars&order=desc")
+    Call<List<Repo>> getRepos();
+
+}
